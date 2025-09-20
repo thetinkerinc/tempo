fd77a58a7f285c85d4f7904496a2788761a5bde2
Shane Brass <shanebrass1618@gmail.com>
2025-09-19 20:26:00 -0600

    Adding scroll indicators to hour selector

--- src/lib/components/hours-picker.svelte
+++ src/lib/components/hours-picker.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 let { value = $bindable() } = $props();
 
+import { fade } from 'svelte/transition';
+import { ChevronLeft, ChevronRight } from '@lucide/svelte';
+
 import { Button } from '$components/ui/button';
 
+let atStart = $state<boolean>(true);
+let atEnd = $state<boolean>(false);
 let hasHalfSelected = $state<boolean>(false);
+let container = $state<HTMLDivElement>();
 
 function isHourSelected(h: number) {
 	return value === h || value === h + 0.5;
@@ -24,16 +30,53 @@ function handleHalf() {
 		hasHalfSelected = true;
 	}
 }
+
+function handleWheel(evt: WheelEvent) {
+	if (!container) {
+		return;
+	}
+	evt.preventDefault();
+	evt.stopPropagation();
+	container.scrollLeft += evt.deltaY;
+}
+
+function handleScroll() {
+	if (!container) {
+		return;
+	}
+	atStart = container.scrollLeft <= 10;
+	atEnd = container.scrollLeft + container.clientWidth >= container.scrollWidth - 10;
+}
 </script>
 
 <div class="flex items-center gap-3">
-	<div class="flex max-w-[250px] items-center gap-1 overflow-x-auto scrollbar-none">
-		{#each { length: 12 }, h}
-			{@const hour = h + 1}
-			<Button variant={isHourSelected(hour) ? 'default' : 'outline'} onclick={handleHour(hour)}>
-				{hour}
-			</Button>
-		{/each}
+	<div class="relative">
+		{#if !atStart}
+			<div
+				class="pointer-events-none absolute left-0 grid h-full w-[40px] content-center justify-start bg-linear-to-r from-white to-transparent"
+				transition:fade>
+				<ChevronLeft />
+			</div>
+		{/if}
+		{#if !atEnd}
+			<div
+				class="pointer-events-none absolute right-0 grid h-full w-[40px] content-center justify-end bg-linear-to-l from-white to-transparent"
+				transition:fade>
+				<ChevronRight />
+			</div>
+		{/if}
+		<div
+			class="flex max-w-[250px] items-center gap-1 overflow-x-auto scrollbar-none"
+			onwheel={handleWheel}
+			onscroll={handleScroll}
+			bind:this={container}>
+			{#each { length: 12 }, h}
+				{@const hour = h + 1}
+				<Button variant={isHourSelected(hour) ? 'default' : 'outline'} onclick={handleHour(hour)}>
+					{hour}
+				</Button>
+			{/each}
+		</div>
 	</div>
 	<Button variant={hasHalfSelected ? 'default' : 'outline'} onclick={handleHalf}>+.5</Button>
 </div>
