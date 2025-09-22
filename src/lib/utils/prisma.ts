01ed336ef13401645d2427a67218ad37f40cf467
Shane Brass <shanebrass1618@gmail.com>
2025-09-22 14:35:32 -0600

    Updating build

--- src/lib/utils/prisma.ts
+++ src/lib/utils/prisma.ts
@@ -1,13 +1,22 @@
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { DATABASE_URL } from '$env/static/private';
 
 import { PrismaClient } from '$prisma/client';
 
+import type { PrismaClient as PrismaClientType } from '@prisma/client';
+
 export type * from '$prisma/client';
 
+let prisma: PrismaClientType;
+
 function getPrisma() {
-	return new PrismaClient({
+	if (prisma) {
+		return prisma;
+	}
+	prisma = new PrismaClient({
 		datasourceUrl: DATABASE_URL
-	});
+	}).$extends(withAccelerate());
+	return prisma;
 }
 
 export { getPrisma };
