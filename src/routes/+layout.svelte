c7305803e3777fd126b68166be2a5676748e2721
Shane Brass <shanebrass1618@gmail.com>
2025-09-22 22:46:50 -0600

    i18n for clerk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 import { ClerkProvider } from 'svelte-clerk';
+import { enUS, esMX } from '@clerk/localizations';
+import { getLocale } from '$paraglide/runtime';
 
 import Header from './header.svelte';
 import Metaballs from './metaballs.svelte';
@@ -7,9 +9,14 @@ import Metaballs from './metaballs.svelte';
 import '../app.css';
 
 let { children } = $props();
+
+const clerkLocale = {
+	en: enUS,
+	es: esMX
+}[getLocale()];
 </script>
 
-<ClerkProvider>
+<ClerkProvider localization={clerkLocale}>
 	<div class="grid min-h-[100dvh] grid-rows-[auto_1fr]">
 		<Header />
 		<div id="bg" class="bg-linear-160 from-[#ffd3ac] to-[#ff8559] px-3 py-10">
