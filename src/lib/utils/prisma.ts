2448d5294244c2c4b2fbfa3b96511a73db77f3eb
Shane Brass <shanebrass1618@gmail.com>
2025-11-13 14:39:30 -0600

    Using local db in dev

--- src/lib/utils/prisma.ts
+++ src/lib/utils/prisma.ts
@@ -1,16 +1,28 @@
 import ws from 'ws';
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { neonConfig } from '@neondatabase/serverless';
+import { dev } from '$app/environment';
 import { DATABASE_URL } from '$env/static/private';
 
 import { PrismaClient } from '$prisma/client';
 
 export type * from '$prisma/client';
 
-neonConfig.webSocketConstructor = ws;
-neonConfig.poolQueryViaFetch = true;
+let prisma: PrismaClient;
 
-const adapter = new PrismaNeon({ connectionString: DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+if (dev) {
+	const pool = new pg.Pool({ connectionString: DATABASE_URL });
+	const adapter = new PrismaPg(pool);
+	prisma = new PrismaClient({ adapter });
+}
+else {
+	neonConfig.webSocketConstructor = ws;
+	neonConfig.poolQueryViaFetch = true;
+
+	const adapter = new PrismaNeon({ connectionString: DATABASE_URL });
+	prisma = new PrismaClient({ adapter });
+}
 
 export { prisma };
