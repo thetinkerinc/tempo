a469e33f1a781d7e1a15292c74d944d508600bed
Shane Brass <shanebrass1618@gmail.com>
2025-11-26 00:12:40 -0600

    Handling wasm in prisma build

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { unwasm } from 'unwasm/plugin';
 
 export default defineConfig({
 	ssr: {
 		noExternal: ['@thetinkerinc/isolocal']
 	},
 	plugins: [
+		unwasm({
+			esmImport: true
+		}),
 		tailwindcss(),
 		sveltekit(),
 		paraglideVitePlugin({
