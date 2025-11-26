8a16eb08420e76019c3290ec783ccade630eaa0e
Shane Brass <shanebrass1618@gmail.com>
2025-11-25 19:48:30 -0600

    prisma

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import { defineConfig } from 'vite';
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
 export default defineConfig({
 	ssr: {
